c334c622e88e5b89a46e503a7274e23d3cb0843f
Vladislav Kosevich <v.kosevich@aiby.com>
2026-02-05 11:18:18 +0300

    feat: set base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === "true" ? "/SaaSoft-test/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
